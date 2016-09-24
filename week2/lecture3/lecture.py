eeba9711a85e6998fc8e9c6aed70484a7fd75736
tiagomestreteixeira <tiago.mestre.teixeira@polarising.com>
2016-09-24 21:07:48 +0100

    Added lecture 4 in week 2, renamed lecture 1 in the same week to 3

--- week2/lecture3/lecture.py
+++ week2/lecture3/lecture.py
@@ -14,3 +14,11 @@ for index in range(len(s)):
 for char in s:
     if char == 'i' or char == 'u':
         print("There is an i")
+
+# Suggestion on comparing floats:
+aFloat = 3.87583
+anotherFloat = 3.875838383838383
+if abs(aFloat - anotherFloat) < 0.00001:
+    print("they are the same numbers")
+else:
+    print("they aren't the same number")
