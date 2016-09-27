51566a428e9d19c5d46fa87ca25b2d82e6c89849
tiagomestreteixeira <tiago.mestre.teixeira@polarising.com>
2016-09-27 22:28:19 +0100

    Added Exercise polysum. closes #10. closes #5

--- week2/lecture4/polysum.py
+++ week2/lecture4/polysum.py
@@ -1,12 +1,12 @@
-import math
+from math import *
 
 
 def polysum(n, s):
     """
     :param n: number of sides
     :param s: length of each side
-    :return: sum of the are and square of the perimeter
+    :return: sum of : - the area and the square of the perimeter
     """
     perimeter_squared = (n * s) ** 2
-    area = (0.25 * n * s * s) / (math.tan(math.pi / n))
+    area = (0.25 * n * s ** 2) / tan(pi / n)
     return round(perimeter_squared + area, 4)
