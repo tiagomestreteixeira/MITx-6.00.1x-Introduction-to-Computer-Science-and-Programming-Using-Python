63ada65c248b8237d61637b1a81c5305e46bc062
tiagomestreteixeira <tiago.mestre.teixeira@polarising.com>
2016-10-03 23:36:10 +0100

    Finished Problem 2 of Problem Set 3. closes #16

--- week3/ProblemSet3/ps3_hangman.py
+++ week3/ProblemSet3/ps3_hangman.py
@@ -57,13 +57,20 @@ def isWordGuessed(secretWord, lettersGuessed):
 
 
 def getGuessedWord(secretWord, lettersGuessed):
-    '''
+    """
     secretWord: string, the word the user is guessing
     lettersGuessed: list, what letters have been guessed so far
     returns: string, comprised of letters and underscores that represents
       what letters in secretWord have been guessed so far.
-    '''
-    # FILL IN YOUR CODE HERE...
+    """
+    guessed_string = ""
+    for letter in secretWord:
+        if letter in lettersGuessed:
+            guessed_string += letter
+        else:
+            guessed_string += "_"
+    return guessed_string
+
 
 
 
