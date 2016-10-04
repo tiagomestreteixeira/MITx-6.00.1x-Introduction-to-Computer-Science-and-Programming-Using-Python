03631835e8c3a3087cbe1fb6eeed765230497a47
tiagomestreteixeira <tiago.mestre.teixeira@polarising.com>
2016-10-04 17:29:53 +0100

    Added applyF_filter function. closes #29

---
+++ week3/miterm/applyF_filterG.py
@@ -0,0 +1,33 @@
+def applyF_filterG(L, f, g):
+    """
+    Assumes L is a list of integers
+    Assume functions f and g are defined for you.
+    f takes in an integer, applies a function, returns another integer
+    g takes in an integer, applies a Boolean function,
+        returns either True or False
+    Mutates L such that, for each element i originally in L, L contains
+        i if g(f(i)) returns True, and no other elements
+    Returns the largest element in the mutated L or -1 if the list is empty
+    """
+    if len(L) == 0:
+        return -1
+
+    l_temp = L[:]
+    for i in l_temp:
+        if not g(f(i)):
+            L.remove(i)
+
+    return max(L)
+
+
+def f(i):
+    return i + 2
+
+
+def g(i):
+    return i > 5
+
+
+L = [0, -10, 5, 6, -4]
+print(applyF_filterG(L, f, g))
+print(L)
