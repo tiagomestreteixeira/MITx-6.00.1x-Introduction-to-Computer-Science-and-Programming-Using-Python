7742b26b2c5466ec0e4a10a18cb46a3861716830
tiagomestreteixeira <tiago.mestre.teixeira@polarising.com>
2016-10-11 00:12:11 +0100

    Finished Problem 5 of Problem Set 4. closes #36

--- week4/ProblemSet4/ps4a.py
+++ week4/ProblemSet4/ps4a.py
@@ -139,24 +139,6 @@ def updateHand(hand, word):
     Assumes that 'hand' has all the letters in word.
     In other words, this assumes that however many times
     a letter appears in 'word', 'hand' has at least as
-    many of that letter in it. 
-
-    Updates the hand: uses up the letters in the given word
-    and returns the new hand, without those letters in it.
-
-    Has no side effects: does not modify hand.
-
-    word: string
-    hand: dictionary (string -> int)    
-    returns: dictionary (string -> int)
-    """
-
-
-def updateHand(hand, word):
-    """
-    Assumes that 'hand' has all the letters in word.
-    In other words, this assumes that however many times
-    a letter appears in 'word', 'hand' has at least as
     many of that letter in it.
 
     Updates the hand: uses up the letters in the given word
@@ -239,34 +221,35 @@ def playHand(hand, wordList, n):
       n: integer (HAND_SIZE; i.e., hand size required for additional points)
       
     """
-    # BEGIN PSEUDOCODE <-- Remove this comment when you code this function; do your coding within the pseudocode (leaving those comments in-place!)
     # Keep track of the total score
-
+    total = 0
     # As long as there are still letters left in the hand:
+    while calculateHandlen(hand) > 0:
+        # Display the hand
+        displayHand(hand)
+        # Ask user for input
+        word_input = str(input("Enter word, or a \".\" to indicate that you are finished: "))
+        # If the input is a single period:
+        if word_input == ".":
+            # End the game (break out of the loop)
+            break
 
-    # Display the hand
+        # Otherwise (the input is not a single period):
 
-    # Ask user for input
-
-    # If the input is a single period:
-
-    # End the game (break out of the loop)
-
-
-    # Otherwise (the input is not a single period):
-
-    # If the word is not valid:
-
-    # Reject invalid word (print a message followed by a blank line)
-
-    # Otherwise (the word is valid):
-
-    # Tell the user how many points the word earned, and the updated total score, in one line followed by a blank line
-
-    # Update the hand
-
-
-    # Game is over (user entered a '.' or ran out of letters), so tell user the total score
+        # If the word is not valid:
+        if not isValidWord(word_input, hand, wordList):
+            # Reject invalid word (print a message followed by a blank line)
+            print("Invalid word, please try again. ")
+        # Otherwise (the word is valid):
+        else:
+            # Tells how many points the word earned, and the updated total score, in one line followed by a blank line
+            total += getWordScore(word_input, n)
+            print(word_input + " earned " + str(getWordScore(word_input, n)) + " points. Total: "
+                  + str(total) + " points")
+            # Update the hand
+            hand = updateHand(hand, word_input)
+            # Game is over (user entered a '.' or ran out of letters), so tell user the total score
+    print("Run out of letters. Total score:  " + str(total) + " points.")
 
 
 #
