b24dba398a9e4867c10e4da2eb8380d16cb1e8a7
tiagomestreteixeira <tiago.mestre.teixeira@polarising.com>
2016-10-14 21:45:50 +0100

    Added the coordinate exercise

--- week5/lecture9/coordinate.py
+++ week5/lecture9/coordinate.py
@@ -5,19 +5,29 @@ Created on Tue May 10 08:18:02 2016
 @author: WELG
 """
 
+
 class Coordinate(object):
     def __init__(self, x, y):
         self.x = x
         self.y = y
+
     def distance(self, other):
-        x_diff_sq = (self.x-other.x)**2
-        y_diff_sq = (self.y-other.y)**2
-        return (x_diff_sq + y_diff_sq)**0.5
+        x_diff_sq = (self.x - other.x) ** 2
+        y_diff_sq = (self.y - other.y) ** 2
+        return (x_diff_sq + y_diff_sq) ** 0.5
+
     def __str__(self):
         return "<" + str(self.x) + "," + str(self.y) + ">"
+
     def __sub__(self, other):
         return Coordinate(self.x - other.x, self.y - other.y)
-        
 
-c = Coordinate(3,4)
-origin = Coordinate(0,0)
+    def __eq__(self, other):
+        return (self.x == other.x) and (self.y == other.y)
+
+    def __repr__(self):
+        return "Coordinate(" + str(self.x) + ", " + str(self.y) + ")"
+
+
+c = Coordinate(3, 4)
+origin = Coordinate(0, 0)
