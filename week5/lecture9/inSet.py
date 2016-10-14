abdba9bda478c72699dec5dfb724d8978dd9ee21
tiagomestreteixeira <tiago.mestre.teixeira@polarising.com>
2016-10-14 21:46:24 +0100

    Added inSet exercise

---
+++ week5/lecture9/inSet.py
@@ -0,0 +1,41 @@
+class intSet(object):
+    """An intSet is a set of integers
+    The value is represented by a list of ints, self.vals.
+    Each int in the set occurs in self.vals exactly once."""
+
+    def __init__(self):
+        """Create an empty set of integers"""
+        self.vals = []
+
+    def insert(self, e):
+        """Assumes e is an integer and inserts e into self"""
+        if not e in self.vals:
+            self.vals.append(e)
+
+    def member(self, e):
+        """Assumes e is an integer
+           Returns True if e is in self, and False otherwise"""
+        return e in self.vals
+
+    def remove(self, e):
+        """Assumes e is an integer and removes e from self
+           Raises ValueError if e is not in self"""
+        try:
+            self.vals.remove(e)
+        except:
+            raise ValueError(str(e) + ' not found')
+
+    def __str__(self):
+        """Returns a string representation of self"""
+        self.vals.sort()
+        return '{' + ','.join([str(e) for e in self.vals]) + '}'
+
+    def intersect(self, other):
+        new_set = intSet()
+        for firstSetItem in self.vals:
+            if other.member(firstSetItem):
+                new_set.insert(firstSetItem)
+        return new_set
+
+    def __len__(self):
+        return len(self.vals)
