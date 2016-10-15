20205d4797c498e9be3997f2b3addd7f04bd6ce3
tiagomestreteixeira <tiago.mestre.teixeira@polarising.com>
2016-10-16 00:47:32 +0100

    replaced ''' with """ of docstrings.

--- week5/lecture10/hand.py
+++ week5/lecture10/hand.py
@@ -3,11 +3,11 @@ import random
 
 class Hand(object):
     def __init__(self, n):
-        '''
+        """
         Initialize a Hand.
 
         n: integer, the size of the hand.
-        '''
+        """
         assert type(n) == int
         self.HAND_SIZE = n
         self.VOWELS = 'aeiou'
@@ -17,9 +17,9 @@ class Hand(object):
         self.dealNewHand()
 
     def dealNewHand(self):
-        '''
+        """
         Deals a new hand, and sets the hand attribute to the new hand.
-        '''
+        """
         # Set self.hand to a new, empty dictionary
         self.hand = {}
 
@@ -35,7 +35,7 @@ class Hand(object):
             self.hand[x] = self.hand.get(x, 0) + 1
 
     def setDummyHand(self, handString):
-        '''
+        """
         Allows you to set a dummy hand. Useful for testing your implementation.
 
         handString: A string of letters you wish to be in the hand. Length of this
@@ -43,7 +43,7 @@ class Hand(object):
 
         This method converts sets the hand attribute to a dictionary
         containing the letters of handString.
-        '''
+        """
         assert len(
             handString) == self.HAND_SIZE, "Length of handString ({0}) must equal length of HAND_SIZE ({1})".format(
             len(handString), self.HAND_SIZE)
@@ -52,18 +52,18 @@ class Hand(object):
             self.hand[char] = self.hand.get(char, 0) + 1
 
     def calculateLen(self):
-        '''
+        """
         Calculate the length of the hand.
-        '''
+        """
         ans = 0
         for k in self.hand:
             ans += self.hand[k]
         return ans
 
     def __str__(self):
-        '''
+        """
         Display a string representation of the hand.
-        '''
+        """
         output = ''
         hand_keys = sorted(self.hand.keys())
         for letter in hand_keys:
