c01712a4995d143fa272f3511e163eab96a237fb
tiagomestreteixeira <tiago.mestre.teixeira@polarising.com>
2016-10-18 18:43:33 +0100

    Completed Problem 2 of Problem Set 5. closes #43

--- week5/ProblemSet5/ps6.py
+++ week5/ProblemSet5/ps6.py
@@ -162,7 +162,10 @@ class PlaintextMessage(Message):
         Hint: consider using the parent class constructor so less
         code is repeated
         """
-        pass  # delete this line and replace with your code here
+        Message.__init__(self, text)
+        self.shift = shift
+        self.encrypting_dict = self.build_shift_dict(shift)
+        self.message_text_encrypted = self.apply_shift(shift)
 
     def get_shift(self):
         """
@@ -170,7 +173,7 @@ class PlaintextMessage(Message):
 
         Returns: self.shift
         """
-        pass  # delete this line and replace with your code here
+        return self.shift
 
     def get_encrypting_dict(self):
         """
@@ -178,7 +181,8 @@ class PlaintextMessage(Message):
 
         Returns: a COPY of self.encrypting_dict
         """
-        pass  # delete this line and replace with your code here
+
+        return dict(self.encrypting_dict)
 
     def get_message_text_encrypted(self):
         """
@@ -186,7 +190,7 @@ class PlaintextMessage(Message):
 
         Returns: self.message_text_encrypted
         """
-        pass  # delete this line and replace with your code here
+        return self.message_text_encrypted
 
     def change_shift(self, shift):
         """
@@ -199,8 +203,9 @@ class PlaintextMessage(Message):
 
         Returns: nothing
         """
-        pass  # delete this line and replace with your code here
-
+        self.shift = shift
+        self.encrypting_dict = self.build_shift_dict(shift)
+        self.message_text_encrypted = self.apply_shift(shift)
 
 class CiphertextMessage(Message):
     def __init__(self, text):
