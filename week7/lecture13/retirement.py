dd966d79b3ef25a96f3afa7e9fe2766b1360f587
tiagomestreteixeira <tiago.mestre.teixeira@polarising.com>
2016-10-24 00:08:21 +0100

    Indented the code of the lecture 13

--- week7/lecture13/retirement.py
+++ week7/lecture13/retirement.py
@@ -6,53 +6,60 @@ Created on Sun Jun 12 14:28:55 2016
 """
 
 import pylab as plt
+plt.interactive(False)
+
 
 def retire(monthly, rate, terms):
     savings = [0]
     base = [0]
-    monthlyRate = rate/12
+    monthlyRate = rate / 12
     for i in range(terms):
         base += [i]
-        savings += [savings[-1]*(1 + monthlyRate) + monthly]
+        savings += [savings[-1] * (1 + monthlyRate) + monthly]
     return base, savings
-    
+
+
 def displayRetirementWithMonthlies(monthlies, rate, terms):
     plt.figure('retireMonth')
     plt.clf()
     for monthly in monthlies:
         xvals, yvals = retire(monthly, rate, terms)
-        plt.plot(xvals, yvals, label = 'retire:'+str(monthly))
-        plt.legend(loc = 'upper left')
+        plt.plot(xvals, yvals, label='retire:' + str(monthly))
+        plt.legend(loc='upper left')
 
-displayRetirementWithMonthlies([500, 600, 700, 800, 900, 1000, 1100], .05, 40* 12)
-    
+displayRetirementWithMonthlies([500, 600, 700, 800, 900, 1000, 1100], .05, 40 * 12)
+
+
 def displayRetirementWithRates(monthly, rates, terms):
     plt.figure('retireRate')
     plt.clf()
     for rate in rates:
         xvals, yvals = retire(monthly, rate, terms)
-        plt.plot(xvals, yvals, 
-                 label = 'retire:'+str(monthly)+ ':' + str(int(rate*100)))
-        plt.legend(loc = 'upper left')
+        plt.plot(xvals, yvals,
+                 label='retire:' + str(monthly) + ':' + str(int(rate * 100)))
+        plt.legend(loc='upper left')
 
-displayRetirementWithRates(800,[.03, .05, .07], 40*12)
 
-      
+displayRetirementWithRates(800, [.03, .05, .07], 40 * 12)
+
+
 def displayRetirementWithMonthsAndRates(monthlies, rates, terms):
     plt.figure('retireBoth')
     plt.clf()
-    plt.xlim(30*12, 40*12)
+    plt.xlim(30 * 12, 40 * 12)
     for monthly in monthlies:
         for rate in rates:
             xvals, yvals = retire(monthly, rate, terms)
             plt.plot(xvals, yvals,
-                     label = 'retire:'+str(monthly)+ ':' + str(int(rate*100)))
-            plt.legend(loc = 'upper left')
-            
-displayRetirementWithMonthsAndRates([500, 700, 900, 1100], 
-                                       [.03, .05, .07], 40*12)
-            
-#def displayRetirementWithMonthsAndRates(monthlies, rates, terms):
+                     label='retire:' + str(monthly) + ':' + str(int(rate * 100)))
+            plt.legend(loc='upper left')
+
+
+displayRetirementWithMonthsAndRates([500, 700, 900, 1100],
+                                    [.03, .05, .07], 40 * 12)
+plt.show()
+
+# def displayRetirementWithMonthsAndRates(monthlies, rates, terms):
 #    plt.figure('retireBoth')
 #    plt.clf()
 #    plt.xlim(30*12, 40*12)
@@ -69,9 +76,6 @@ displayRetirementWithMonthsAndRates([500, 700, 900, 1100],
 #                     label = 'retire:'+str(monthly)+ ':' + str(int(rate*100)))
 #            plt.legend(loc = 'upper left')
 #
-#displayRetirementWithMonthsAndRates([500, 700, 900, 1100], 
+# displayRetirementWithMonthsAndRates([500, 700, 900, 1100],
 #                                       [.03, .05, .07], 40*12)
-#  
-  
-
-        
+#
